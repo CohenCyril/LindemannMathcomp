7a6a2061d626f2d9a8a878b8aff4ff12ed188d72
SBernard <sophie.bernards@gmail.com>
2016-10-13 19:49:12 +0200

    ajout tex preuve et structures numArchi... et realArchiDomain fonctionnelles, a priori

---
+++ lind.v
@@ -0,0 +1,22 @@
+From mathcomp Require Import all_ssreflect algC.
+
+(*Section Analyse.
+
+End Analyse.
+
+Section LemmeA.
+
+End LemmeA.*)
+
+Section Lindemann.
+
+Variable l : nat.
+Variable A : l.-tuple algC.
+Hypothesis A_uniq : uniq A.
+Variable a : l.-tuple algC.
+Hypothesis a_neq0 :
+
+
+End Lindemann. 
+
+
